486caedcd7abbefb6fdac795c7928828c0c86068
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2024-10-20 06:43:10 +0900

    [feat]: buggy setup for wordpress, nginx, and mariadb

---
+++ inception/srcs/requirements/wordpress/test.sh
@@ -0,0 +1,15 @@
+php-fpm7.4 -F
+wp config create	--dbhost=$WPDB_HOST \
+					--dbname=$DB_NAME \
+					--dbuser=$DB_USER \
+					--dbpass=$DB_PASS \
+					--allow-root
+wp core install	---url=$DOMAIN \
+				--title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_EMAIL \
+				--skip-email \
+				--allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD
+php-fpm7.4 -F
