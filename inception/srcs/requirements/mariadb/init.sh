de9b42edfe8d62eb6ec0f547b884fc55af39838e
maria <winterzaq1@gmail.com>
2024-10-20 17:37:24 +0900

    [fix]: mount volume to nginx, other slight edits

--- inception/srcs/requirements/mariadb/init.sh
+++ inception/srcs/requirements/mariadb/init.sh
@@ -1,4 +1,7 @@
+mkdir -p /etc/my.cnf.d
+
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > /etc/my.cnf.d/init.txt
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> /etc/my.cnf.d/init.txt
 echo "FLUSH PRIVILEGES;" >> /etc/my.cnf.d/init.txt
+
 mariadbd --init-file=/etc/my.cnf.d/init.txt
