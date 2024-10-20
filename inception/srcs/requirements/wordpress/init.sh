d19bb849199b47bbf3a197fa9af229e878f5b5c6
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2024-10-20 17:37:24 +0900

    [fix]: mount volume to nginx, other slight edits

--- inception/srcs/requirements/wordpress/init.sh
+++ inception/srcs/requirements/wordpress/init.sh
@@ -1,1 +1,20 @@
+set -x
+cd /var/www/html
+wp config create	--dbhost=$WPDB_HOST \
+					--dbname=$DB_NAME \
+					--dbuser=$DB_USER \
+					--dbpass=$DB_PASSWORD \
+					--allow-root
+wp core install	--url=$DOMAIN \
+				--title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_EMAIL \
+				--skip-email \
+				--allow-root
+wp user create	$WP_USER \
+				$WP_USER_EMAIL \
+				--role=author \
+				--user_pass=$WP_USER_PASSWORD \
+				--allow-root
 php-fpm7.4 -F
